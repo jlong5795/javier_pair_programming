c3a4859d300192db081b4d8749c0da68516e8166
Javier Ibanez <expertprep2015@gmail.com>
2021-11-15 22:06:26 -0500

    step1-4 pseudo code

--- MakeArrayConsecutive.js
+++ MakeArrayConsecutive.js
@@ -5,7 +5,15 @@ Ratiorg got statues of different sizes as a present from CodeMaster for his birt
 const statues = [6, 2, 3, 8];
 
 function solution (statues) {
+    // step 1: Sort the statues from smallest to largest
+    
+    // step 2: Loop through the array of statues [2, 3, 6, 8]
 
+    // step 3: evaluate the current number +1 and see if it equals the next number
+
+    // step 4: If when we add +1 to our current number and it equals the next number, continue to the next number in the for loop
+
+    // step 4b: If they're not equal, create a list of missing statues
 };
 
-console.log(solution(statutes))
+console.log(solution(statues))
